9bcfc28e002ca729acba4ff9f42db1c434504a19
Alex Huang <alexh5@uw.edu>
2018-05-21 09:52:04 -0700

    add alcohol abuse disorder to shiny app

--- server.R
+++ server.R
@@ -1,17 +1,6 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-
 library(shiny)
 library(plotly)
 library(rsconnect)
-
-# Read in NSDUH data (Must extract NSDUH.tsv)
-#nsduh_data <- read.csv("data/raw/NSDUH/NSDUH.tsv", sep = "\t")
 
 # Read in GBD data
 gbd_data <- read.csv("data/raw/GBD.csv")
@@ -23,6 +12,76 @@ state_alc_taxrate <- na.omit(state_alc_taxrate)
 state_alc_taxrate <- filter(state_alc_taxrate, DataValueType != "Age-adjusted Prevalence", 
                             DataValueType != "Age-adjusted Mean", 
                             DataValueType != "Age-adjusted Rate")
+
+# Read in data
+
+# Table 21. Alcohol Use Disorder in the Past Year, by Age Group and State:
+# Estimated Numbers (in Thousands), Annual Averages Based on 2015 and 2016 NSDUHs
+alcohol_data <- read.csv("data/raw/NSDUHsaeTotals2016-CSVs/alcohol_use_disorder.csv",
+                         stringsAsFactors = F)
+
+# Estimated 2016 U.S. state populations by age groups
+
+population_data <- read.csv("data/raw/PEP_2016_PEPAGESEX_with_ann.csv", stringsAsFactors = F)
+
+# Wrangle data
+
+alcohol_data[1, 2] <- "United States"
+alcohol_data <- alcohol_data[-1 : -5, ]
+
+alcohol_data$Age_Over_11 <- 
+  as.numeric(gsub(",", "", alcohol_data$X12.or.Older.Estimate)) * 1000
+alcohol_data$Age_Over_17 <- 
+  as.numeric(gsub(",", "", alcohol_data$X18.or.Older.Estimate)) * 1000
+alcohol_data$Age_Over_25 <-
+  as.numeric(gsub(",", "", alcohol_data$X26.or.Older.Estimate)) * 1000
+
+alcohol_data <- select(alcohol_data, State, Age_Over_11, Age_Over_17, Age_Over_25)
+
+population_data <- select(population_data, GEO.display.label, est72016sex0_age10to14, 
+                          est72016sex0_age15to19, est72016sex0_age20to24, est72016sex0_age25to29,
+                          est72016sex0_age30to34, est72016sex0_age35to39, est72016sex0_age40to44,
+                          est72016sex0_age45to49, est72016sex0_age50to54, est72016sex0_age55to59,
+                          est72016sex0_age60to64, est72016sex0_age65to69, est72016sex0_age70to74,
+                          est72016sex0_age75to79,est72016sex0_age80to84, est72016sex0_age85plus)
+
+population_data <- population_data[-1, ]
+
+for(i in c(2 : ncol(population_data))) {
+  population_data[ , i] <- as.numeric(population_data[ , i])
+}
+
+population_data <- mutate(population_data, Total_Age_Over_11 = (est72016sex0_age10to14 / 2) + est72016sex0_age15to19 + 
+                            est72016sex0_age20to24 +  est72016sex0_age25to29 + est72016sex0_age30to34 +
+                            est72016sex0_age35to39 +  est72016sex0_age40to44 + est72016sex0_age45to49 +
+                            est72016sex0_age50to54 +  est72016sex0_age55to59 + est72016sex0_age60to64 +
+                            est72016sex0_age65to69 +  est72016sex0_age70to74 + est72016sex0_age75to79 + 
+                            est72016sex0_age80to84 +  est72016sex0_age85plus,
+                          Total_Age_Over_17 = Total_Age_Over_11 - (est72016sex0_age10to14 / 2) - 
+                            (est72016sex0_age15to19 / 2),
+                          Total_Age_Over_25 = Total_Age_Over_17 - (est72016sex0_age15to19 / 2) - 
+                            est72016sex0_age20to24) %>%
+  select(GEO.display.label, Total_Age_Over_11, Total_Age_Over_17, Total_Age_Over_25)
+
+colnames(population_data)[1] <- "State"
+
+joined_data <- left_join(alcohol_data, population_data) %>%
+  mutate(Prop_Age_Over_11 = Age_Over_11 / Total_Age_Over_11,
+         Prop_Age_Over_17 = Age_Over_17 / Total_Age_Over_17,
+         Prop_Age_Over_25 = Age_Over_25 / Total_Age_Over_25)
+
+over_11_count <- select(joined_data, State, Age_Over_11)
+colnames(over_11_count)[2] <- "Number"
+over_11_prop <- select(joined_data, State, Prop_Age_Over_11)
+colnames(over_11_prop)[2] <- "Number"
+over_17_count <- select(joined_data, State, Age_Over_17)
+colnames(over_17_count)[2] <- "Number"
+over_17_prop <- select(joined_data, State, Prop_Age_Over_17)
+colnames(over_17_prop)[2] <- "Number"
+over_25_count <- select(joined_data, State, Age_Over_25)
+colnames(over_25_count)[2] <- "Number"
+over_25_prop <- select(joined_data, State, Prop_Age_Over_25)
+colnames(over_25_prop)[2] <- "Number"
 
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output, session) {
@@ -92,6 +151,44 @@ server <- shinyServer(function(input, output, session) {
                          y = fitted(lm(tax_rate ~ DataValueAlt, filtered_state_alc_taxrate)),
                          mode = "lines"
       )
+    })
+    
+    output$plot <- renderPlotly({
+      
+      # target_data <- over_11_count
+      
+      if(input$ageFilter == "12 and Older" & input$typeFilter == "Count") {
+        target_data <- over_11_count
+        title.label <- "Number of Individuals Age 12 and Older with Alcohol Use Disorder"
+        y.label <- "Number of Individuals"
+      } else if (input$ageFilter == "12 and Older" & input$typeFilter == "Proportion") {
+        target_data <- over_11_prop
+        title.label <- "Proportion of Individuals Age 12 and Older with Alcohol Use Disorder"
+        y.label <- "Proportion of Individuals"
+      } else if (input$ageFilter == "18 and Older" & input$typeFilter == "Count") {
+        target_data <- over_17_count
+        title.label <- "Number of Individuals Age 18 and Older with Alcohol Use Disorder"
+        y.label <- "Number of Individuals"
+      } else if (input$ageFilter == "18 and Older" & input$typeFilter == "Proportion") {
+        target_data <- over_17_prop
+        title.label <- "Proportion of Individuals Age 18 and Older with Alcohol Use Disorder"
+        y.label <- "Proportion of Individuals"
+      } else if (input$ageFilter == "26 and Older" & input$typeFilter == "Count") {
+        target_data <- over_25_count
+        title.label <- "Number of Individuals Age 26 and Older with Alcohol Use Disorder"
+        y.label <- "Number of Individuals"
+      } else if (input$ageFilter == "26 and Older" & input$typeFilter == "Proportion") {
+        target_data <- over_25_prop
+        title.label <- "Proportion of Individuals Age 26 and Older with Alcohol Use Disorder"
+        y.label <- "Proportion of Individuals"
+      }
+      
+      graph <- plot_ly(data = target_data, x = ~State, y = ~Number, type = "bar") %>%
+        layout(
+          title = title.label,
+          yaxis = list(title = y.label)
+        )
+      
     })
     
 })
--- ui.R
+++ ui.R
@@ -14,36 +14,71 @@ ui <- shinyUI(fluidPage(
   # Application title
   titlePanel("Drinking vs Tax Rates"),
   
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("Question", "Question",
-                  choices = unique(state_alc_taxrate$Question) 
-      ),
-      
-      sliderInput("year", label = h3("year"), min = NA, 
-                  max = NA, value = c(NA,NA)
-      ),
-      
-      selectInput("strata", "strata",
-                  choices = "",
-                  selected = ""
-      ),
-      selectInput("by", "by",
-                  choices = "",
-                  selected = ""
-      ),
-      
-      selectInput("type", "type",
-                  choices = "",
-                  selected = ""
-      )
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotlyOutput('state_alc_taxrate_plot', height = "900px")
-    )
+  navbarPage("",
+             
+             tabPanel("Kevin",
+                
+                      sidebarLayout(
+                        
+                        sidebarPanel(
+                          
+                          selectInput("Question", "Question",
+                                      choices = unique(state_alc_taxrate$Question) 
+                                      ),
+                          
+                          sliderInput("year", label = h3("year"), min = NA,
+                                      max = NA, value = c(NA,NA)
+                                      ),
+                          
+                          selectInput("strata", "strata", choices = "", 
+                                      selected = ""
+                                      ),
+                          
+                          selectInput("by", "by", choices = "", selected = ""
+                                      ),
+                          
+                          selectInput("type", "type", choices = "", selected = ""
+                                      )
+                        ),
+                        
+                        # Show a plot of the generated distribution
+                        mainPanel(
+                          plotlyOutput('state_alc_taxrate_plot', height = "900px")
+                        )
+                      )
+                    ),
+             
+             tabPanel("Alex: Alcohol Use Disorder in the United States 2015-2016",
+                      sidebarLayout(
+                        
+                        sidebarPanel(
+                          
+                          selectInput("ageFilter", label = "Choose the Age Range",
+                                      choices = c("12 and Older", "18 and Older", "26 and Older"),
+                                      selected = "12 and Older"),
+                          
+                          radioButtons("typeFilter", "Count or Proportion",
+                                       c("Count", "Proportion"),
+                                       selected = "Count")
+                          
+                        ),
+                        
+                        
+                        mainPanel(
+                          
+                          plotlyOutput("plot")
+                        )
+                      )
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      )
+            )
   )
-))
+)
 
 shinyUI(ui)
